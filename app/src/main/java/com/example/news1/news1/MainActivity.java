9ce83ac46a8649b3b769b4e8701bcbfa2a8684a6
colt005 <rohan.prabhu05@gmail.com>
2018-06-15 23:20:14 +0530

    Added Asynctask

--- app/src/main/java/com/example/news1/news1/MainActivity.java
+++ app/src/main/java/com/example/news1/news1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.news1.news1;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -23,6 +24,8 @@ import static com.example.news1.news1.R.id.action_settings;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -30,58 +33,82 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnHit = (Button) findViewById(R.id.btnHit);
-        final TextView tvData = (TextView) findViewById(R.id.tvJsonItem);
+        tvData = (TextView) findViewById(R.id.tvJsonItem);
 
         btnHit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                HttpURLConnection connection = null;
-                BufferedReader reader = null;
 
-                try {
-                    URL url = new URL("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + R.string.API_KEY);
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.connect();
+                //new JSONTask().execute("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + R.string.API_KEY);
+                new JSONTask().execute("https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/moviesDemoItem.txt");
 
-                    InputStream stream = connection.getInputStream();
-
-                    reader = new BufferedReader(new InputStreamReader(stream));
-
-                    StringBuffer buffer = new StringBuffer();
-
-                    String line = " ";
-                    while ((line = reader.readLine()) != null) {
-
-                        buffer.append(line);
-                    }
-
-                    tvData.setText(buffer.toString());
-
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-
-                    if (connection != null) {
-                        connection.disconnect();
-                    }
-                    try {
-                        if (reader != null) {
-                            reader.close();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                }
 
             }
         });
 
 
     }
+
+
+    public  class JSONTask extends AsyncTask<String, String, String>{
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(urls[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = " ";
+                while ((line = reader.readLine()) != null) {
+
+                    buffer.append(line);
+                }
+
+                return buffer.toString();
+
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            tvData.setText(result);
+
+        }
+    }
+
 
 
     public boolean onCreateOptionsMenu(Menu menu) {
