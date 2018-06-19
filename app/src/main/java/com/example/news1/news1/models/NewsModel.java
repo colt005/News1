9209853bea226b4058ef5fe3ab91520faa3714df
colt005 <rohan.prabhu05@gmail.com>
2018-06-19 23:17:11 +0530

    Added Model.java

---
+++ app/src/main/java/com/example/news1/news1/models/NewsModel.java
@@ -0,0 +1,95 @@
+package com.example.news1.news1.models;
+
+import java.util.List;
+
+public class NewsModel {
+
+    private List<source> sourceList;
+    private String author;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+
+    public List<source> getSourceList() {
+        return sourceList;
+    }
+
+    public void setSourceList(List<source> sourceList) {
+        this.sourceList = sourceList;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public static class source{
+
+        private String id;
+        private String name;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+}
+
